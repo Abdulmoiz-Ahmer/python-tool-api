344791ec63d082fcc2a68fed2973a653d5d1e1d0
Abdul Wajid <54894273+abdulwajid887@users.noreply.github.com>
2021-11-15 20:29:43 +0500

    Update api.py

--- api.py
+++ api.py
@@ -131,7 +131,173 @@ def getDigitalEyeAttributes(att_inp: str) -> Dict:
         print("172", e)
     return response
     # return "DigEyeatt"
+@app.route('/api/nft/<string:coll_inp>/<string:att_inp>/<float:price_inp>', methods=['GET'])
+def get_response(coll_inp, att_inp, price_inp)-> Dict:
+    ret_dic_nft = dict()
 
+    # price = 14
+    price = price_inp
+
+    # SOLANART FILTERING
+    # set to True to check if att_value is in attributes of the nft
+    filter_solanart = False
+
+    # value of the attribute
+    # att_value = "Zombie"
+    att_value = att_inp
+
+    trait_type = "Title"
+    value = "Monitor"
+    
+    collection = [coll_inp, coll_inp]
+
+    check_solanaArt = True
+    check_DigitalEye = True
+
+    if collection[0] is not None:
+        print("\n\n\nstarted parsing solanart")
+        try:
+            # first_solanart = getSetIdSolanart(collection[0])
+            first_solanart = "fds"
+            if first_solanart == "Collection not found":
+                check_solanaArt = False
+        except:
+            print("91", "Error on initial check")
+
+    print("parsed initial")
+
+    notify = False
+
+    msg = "new data parsed for " + \
+        str(collection[0]) + \
+        " for maximum : " + str(price)
+    if filter_solanart:
+        msg = msg + " filtering for :" + att_value + " on solanart"
+    if trait_type is not None:
+        msg = msg + " filtering for " + value + " on digieyes"
+    ret_dic_nft["msg"] = msg
+
+    ret_dic_nft["resp_Solanart"] = []
+    if collection[0] is not None and check_solanaArt:
+        print("183", "Into collection[0]")
+        try:
+            # second_solanart = getSetIdSolanart(collection[0])
+            second_solanart = api_calls.getSetIdSolanart(collection[0])
+            # second_solanart = second
+            print("169 -------------------")
+            # print(second_solanart)
+            # try:
+            #     diff_solanart = second_solanart.keys() - first_solanart.keys()
+            # except Exception as e:
+            #     print("173 :", e)
+            print("171 ===============")
+            if len(second_solanart) == 0:
+            # if len(diff_solanart) == 0:
+                print("Solanart: No new key")
+                # ret_dic_nft["resp_Solanart"].append("No new key")
+            else:
+                print("173, fgnafln")
+                for elem in second_solanart:
+                    try:
+                        nft: Dict = second_solanart.get(elem)
+                    except:
+                        nft: Dict = second_solanart['elem']
+                    # print('206 ///////////////////')
+                    if nft.get("price") < price:
+                        res_URL ="https://qzlsklfacc.medianetwork.cloud/nft_for_sale?collection=" + collection[0]
+                        if filter_solanart:
+                            if att_value in nft.get("attributes"):
+                                # ret_dic_nft["resp_Solanart"] = notification()
+                                ret_dic_nft["resp_Solanart"].append(nft.get("name") + " -- " +nft.get("link_img"))
+                            else:
+                                # ret_dic_nft["resp_Solanart"].append("Attribute does not fit")
+                                pass
+                        else:
+                            # ret_dic_nft["resp_Solanart"] = notification()
+                            ret_dic_nft["resp_Solanart"].append(nft.get("name") + " -- " +nft.get("link_img"))
+                    else:
+                        # ret_dic_nft["resp_Solanart"].append("Price does not fit")
+                        pass
+        except Exception as e:
+            print("196", e)
+            # ret_dic_nft["resp_Solanart"].append("ERROR: data source unavailable")
+    else:
+        return_statement = "No Collection named '"+ collection[0] + "' in SolanaArt"
+        print("Solanart:",return_statement)
+        # ret_dic_nft["resp_Solanart"].append(return_statement)
+
+    if collection[1] is not None:
+        print("\n\n\nstarted parsing digitalEyes")
+        # first_digeye = api_calls.getSetIdDigitalEyes(collection[1])
+        first_digeye = 'bkh'
+        if first_digeye == "Collection not found":
+            check_DigitalEye = False
+
+    ret_dic_nft["resp_DigitalEye"] = []
+    if collection[1] is not None and check_DigitalEye:
+        print("210", "Into collection[1]")
+        try:
+            second_digeye = api_calls.getSetIdDigitalEyes(collection[1])
+            print("242 -------------------")
+            # print("127", second_digeye.keys())
+            # diff_digieye = second_digeye.keys() - first_digeye.keys()
+            print("245 ===============")
+            if len(second_digeye) == 0:
+            # if len(second_digeye) == 0:
+                print("DigitalEye: No new Key")
+                # ret_dic_nft["resp_DigitalEye"].append("No new key")
+            else:
+                for elem in second_digeye:
+                    try:
+                        nft: Dict = second_digeye.get(elem)
+                    except:
+                        nft: Dict = second_digeye['elem']                    
+                    # print('256 ///////////////////')
+                    # print("167", nft["price"]/DIGITAL_CONSTANT)
+                    if nft["price"] < price * DIGITAL_CONSTANT:
+                        ret_dic_nft["resp_DigitalEye"].append(nft["metadata"]["name"] + " -- " + nft["metadata"]["image"])
+
+                        
+                        # print('259 ..........................')
+                        # print("169", nft["price"]/DIGITAL_CONSTANT)
+                        # if trait_type is not None:
+                        #     # print('262 ,,,,,,,,,,,,,,,,,,,,,,,', nft["metadata"]["attributes"])
+                        #     for att in nft["metadata"]:
+                        #         # try:
+                        #         #     print('264 ..............', att)
+                        #         # except:
+                        #         #     print('267 ..............', nft.get("attributes"))
+                        #         for att_val in nft["metadata"]["attributes"]:
+                        #             print(att_val['value'])
+                        #         if nft["metadata"]["attributes"]["trait_type"] == trait_type and nft["metadata"]["attributes"]["value"] == value:
+                        #             # print('266 ,,,,,,,')
+                        #             # ret_dic_nft["resp_DigitalEye"].append(res_URl)
+                        #             ret_dic_nft["resp_DigitalEye"].append(att["name"] + " -- " + att["image"])
+                        #         else:
+                        #             pass
+                        #             # ret_dic_nft["resp_DigitalEye"].append("Trait or attribute does not match")
+                        # else:
+                        #     # ret_dic_nft["resp_DigitalEye"].append(res_URl)
+                        #     ret_dic_nft["resp_DigitalEye"].append(nft["metadata"]["name"] + " -- " + nft["metadata"]["image"])
+                    else:
+                        pass
+                        # ret_dic_nft["resp_DigitalEye"].append("Price does not fit")
+        except Exception as e:
+            print("279", e)
+            # ret_dic_nft["resp_DigitalEye"].append("ERROR: data source unavailable")
+    else:
+        return_statement = "No Collection named '"+ collection[0] + "' in DigitalEye"
+        print("DigitalEye:",return_statement)
+        # ret_dic_nft["resp_DigitalEye"].append(return_statement)
+
+    response = flask.jsonify({'NFT': ret_dic_nft})
+    print("287", response)
+    try:
+        response.headers.add('Access-Control-Allow-Origin', '*')
+    except Exception as e:
+        print("292", e)
+    return response
+    
 @app.route('/api/nft/<string:coll_inp>/<string:att_inp>/<int:price_inp>', methods=['GET'])
 def get_response(coll_inp, att_inp, price_inp)-> Dict:
     ret_dic_nft = dict()

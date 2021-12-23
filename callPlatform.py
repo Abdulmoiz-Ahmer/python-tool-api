52014496f3de101201d6ece25dfb57d50bd81fda
Abdul Wajid <54894273+abdulwajid887@users.noreply.github.com>
2021-12-23 21:38:45 +0500

    Update callPlatform.py

--- callPlatform.py
+++ callPlatform.py
@@ -3,12 +3,35 @@ import urllib.request
 from typing import List, Dict, Set
 import requests
 
+def read_text_file(file_name):
+    f = open(file_name, 'r', encoding='Latin1')
+    file_text = f.readlines()
+    return file_text
+
+def get_list_ofcoll_link(collec_name):
+    list1 = read_text_file("coll_link_solanart.txt")
+    ret_coll = "not_found"
+    for x in list1:
+        coll_name = x.split('~:')[0]
+        link_name = x.split('~:')[1].replace('\n', '')
+        # print(coll_name, link_name)
+        if collec_name == coll_name:
+            ret_coll =  link_name
+            break
+
+    if not ret_coll == "not_found":
+        return ret_coll
+    else:
+        return collec_name
+
 def getSetIdSolanart(collection: str) -> Dict:
     print("Start Solanart")
     SetIdSol = dict()
+    # collection = collection.lower()
     # url = "https://jmccmlyu33.medianetwork.cloud/nft_for_sale?collection=" + collection
+    collection = get_list_ofcoll_link(collection)
     url = "https://qzlsklfacc.medianetwork.cloud/nft_for_sale?collection=" + collection
-    url = url.replace(" ", "").replace("&", "and").replace("2D Soldiers", "solarmy2d").replace("3D Soldiers", "solarmy3d").lower()
+    # url = url.replace(" ", "").replace("&", "and").replace("2D Soldiers", "solarmy2d").replace("3D Soldiers", "solarmy3d").lower()
     try:
         counter= 0
         for elem in json.loads(urllib.request.urlopen(url).read()):

3b3ffa7762dac5229d14f31251687a328855b001
Abdul Wajid <54894273+abdulwajid887@users.noreply.github.com>
2022-01-25 02:25:11 +0500

    Update callPlatform.py

--- callPlatform.py
+++ callPlatform.py
@@ -18,26 +18,29 @@ def read_text_file(file_name):
     file_text = f.readlines()
     return file_text
 
-def get_list_ofcoll_link(filename, collec_name):
-    list1 = read_text_file(filename)
+def get_list_ofcoll_link(Collenction, collec_name):
+
+    list_coll_name = requests.get('https://cryptobros-76836-default-rtdb.firebaseio.com/Collections/'+Collenction+'.json')
     ret_coll = "not_found"
-    for x in list1:
-        coll_name = x.split('~:')[0]
-        link_name = x.split('~:')[1].replace('\n', '')
+    for x in list_coll_name.json():
+        coll_name = list_coll_name.json()[x]["name"]
+        link_name = list_coll_name.json()[x]["link"]
         # print(coll_name, link_name)
         if collec_name == coll_name:
             ret_coll =  link_name
             break
 
     if not ret_coll == "not_found":
+        # print('34')
         return ret_coll
     else:
+        # print('37')
         return collec_name
     
 def getSetIdMagicEden(collection: str) -> Dict:
     print("Start Magic Eden")
     SetIdMgc = dict()
-    collection = get_list_ofcoll_link("coll_link_magicEden.txt",collection)
+    collection = get_list_ofcoll_link("Magiceden",collection)
     url_test = "https://api-mainnet.magiceden.io/rpc/getListedNFTsByQuery?q={%22$match%22:{%22collectionSymbol%22:%22"+collection+"%22},%22$sort%22:{%22createdAt%22:-1},%22$skip%22:0,%22$limit%22:20}"
     r = requests.get(url_test,
                         headers=headers,
@@ -61,7 +64,7 @@ def getSetIdSolanart(collection: str) -> Dict:
     SetIdSol = dict()
     # collection = collection.lower()
     # url = "https://jmccmlyu33.medianetwork.cloud/nft_for_sale?collection=" + collection
-    collection = get_list_ofcoll_link("coll_link_solanart.txt",collection)
+    collection = get_list_ofcoll_link("Solanart",collection)
     # url = "https://qzlsklfacc.medianetwork.cloud/nft_for_sale?collection=" + collection
     url_test = "https://qzlsklfacc.medianetwork.cloud/get_nft?collection="+collection+"&page=0&limit=100&order=&fits=any&trait=&search=&min=0&max=0&listed=true&ownedby=&attrib_count="
     total_pages = json.loads(urllib.request.urlopen(url_test).read())["pagination"]["maxPages"]
